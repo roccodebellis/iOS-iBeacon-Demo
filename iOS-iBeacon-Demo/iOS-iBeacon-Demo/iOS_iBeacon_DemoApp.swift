aa6b41f6fc6044db13e5757f6d539fa5c8ccb77f
Rocco Debellis <rocdebellis@gmail.com>
2024-10-05 16:07:00 +0200

    Organized project structure and added initial project files

---
+++ iOS-iBeacon-Demo/iOS-iBeacon-Demo/iOS_iBeacon_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_iBeacon_DemoApp.swift
+//  iOS-iBeacon-Demo
+//
+//  Created by r.debellis on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_iBeacon_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
